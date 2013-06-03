c6f58cc4385dc83761617a2bd269e35f0afbefdf
thelightwasbrighter <unterhosenmoerder@gmx.de>
2013-06-04 00:12:05 +0200

    networking

--- ais/bla.py
+++ ais/bla.py
@@ -3,18 +3,37 @@ import math
 import random
 
 class ProbeAi(object):
+    
+    def distance(self, a, b):
+        return (b[0]-a[0], b[1]-a[1])
+    
+    def betrag(self, vect):
+        return math.sqrt(math.pow(vect[0],2)+math.pow(vect[1],2))
+
     def act(self, view):
         #print view.cargo
         #view.cargo['ressources']=[100,100,100]
+    
         if view.landed:
             res=view.cargo['ressources']
-            if res[0]>=10000 and res[1]>=10000 and res[2]>=10000 and view.landed:
+            if res[0]>=von_neumann.PROBE_COST and res[1]>=von_neumann.PROBE_COST and res[2]>=von_neumann.PROBE_COST and view.landed:
                 return von_neumann.Action(von_neumann.ACT_BUILD, 'probe')
             else:
                 return von_neumann.Action(von_neumann.ACT_IDLE)
         else:
-            x=random.uniform(-1,1)
-            y=random.uniform(-1,1)
-            betrag=math.sqrt(math.pow(x,2)+math.pow(y,2))
-            vect=[x/betrag,y/betrag]
-            return von_neumann.Action(von_neumann.ACT_MOVE, vect)
+            empty_planets = []
+            for planet in view.scans['planets']:
+                if planet['populated']==False:
+                    empty_planets.append((planet, self.distance(view.pos, planet['pos'])))
+            if len(empty_planets)==0:
+                x=random.uniform(-1,1)
+                y=random.uniform(-1,1)
+                vect=[x,y]
+                return von_neumann.Action(von_neumann.ACT_MOVE, vect)
+            else:
+                for (planet,dist) in empty_planets:
+                    print "ALARM!!!"
+                    if planet['pos'][0]==view.sector[0] and planet['pos'][1]==view.sector[1] :
+                        return von_neumann.Action(von_neumann.ACT_COLONIZE, None)
+                    else:
+                        return von_neumann.Action(von_neumann.ACT_MOVE, dist)
--- von-neumann.py
+++ von-neumann.py
@@ -21,7 +21,9 @@ PLANETS = 300
 SCALE = 2
 RES_MAX = 100
 CARGO_SLOTS = 10000
-PROBE_COST = 100
+PROBE_COST = 1000
+PROBE_RANGE = 1
+PLANET_RANGE = 5
 
 
 class MapLayer(object):
@@ -72,7 +74,7 @@ class Display(object):
             else:
                 for x in range(0,SCALE):
                     for y in range(0,SCALE):
-                        self.planet_surface.set_at((SCALE*p.get_pos()[0]+x,SCALE*p.get_pos()[1]+y),(25,255,255))
+                        self.planet_surface.set_at((SCALE*p.get_pos()[0]+x,SCALE*p.get_pos()[1]+y),(255,255,255))
         
         for p in probe_list:
             self.probe_surface.set_at((int(math.floor(p.get_pos()[0]))*SCALE,int(math.floor(p.get_pos()[1]))*SCALE),p.get_team().get_colour())
@@ -95,7 +97,7 @@ class Display(object):
 
 
 # Actions available to an agent on each turn.
-ACT_BUILD, ACT_MOVE, ACT_IDLE = range(3)
+ACT_BUILD, ACT_MOVE, ACT_COLONIZE, ACT_IDLE = range(4)
 
 class Action(object):
     '''
@@ -133,6 +135,12 @@ class Planet(object):
     def get_res(self):
         return self.ressource
 
+    def scanned(self):
+        if self.populated:
+            return {'pos':self.pos, 'res':self.ressource, 'populated':self.populated, 'team_id':self.probe.get_team().get_id()}
+        else:
+            return {'pos':self.pos, 'res':self.ressource, 'populated':self.populated, 'team_id':None}
+            
     def set_res(self, res):
         self.ressource=res
 
@@ -145,7 +153,53 @@ class Planet(object):
         else:
             pass
 
+def assign_networks(grid, probe_list):
+    probe_check=[]
+    id_count=0
+    temp_id=0
+    for n in probe_list:
+        probe_check.append(False)
+    while probe_check.count(False)>0:
+        init_probe=probe_list[probe_check.index(False)]
+        check_grid=[]
+        for x in range(UNIVERSE_WIDTH):
+            check_grid.append([])
+            for y in range(UNIVERSE_HEIGHT):
+                check_grid[x].append(False)
+        probe_queue=[init_probe]
+        probe_list_temp=[]
+        while len(probe_queue)!=0:
+            #print probe_queue
+            probe_temp=probe_queue.pop()
+            for x in range(-PROBE_RANGE,PROBE_RANGE):
+                if probe_temp.get_sector()[0]+x>=0 and probe_temp.get_sector()[0]+x<UNIVERSE_WIDTH:
+                    for y in range(-PROBE_RANGE,PROBE_RANGE):
+                        if probe_temp.get_sector()[1]+y>=0 and probe_temp.get_sector()[1]+y<UNIVERSE_HEIGHT:
+                            if check_grid[probe_temp.get_sector()[0]+x][probe_temp.get_sector()[1]+y]==False:
+                                for p in grid[probe_temp.get_sector()[0]+x][probe_temp.get_sector()[1]+y]['probes']:
+                                    if p.get_team()==probe_temp.get_team():
+                                        if probe_check[probe_list.index(p)]==True:
+                                            for i in range(len(probe_list)):
+                                                if probe_check[i]==True:
+                                                    if probe_list[i].get_net_id()==p.get_net_id():
+                                                        probe_list[i].set_net_id(temp_id)
+                                        else:
+                                            probe_list_temp.append(p)
+                                            if p!=probe_temp:
+                                                probe_queue.append(p)
+                                            #check_grid[p.get_sector()[0]][p.get_sector()[1]]=True
+                                check_grid[probe_temp.get_sector()[0]+x][probe_temp.get_sector()[1]+y]=True
+        for p in probe_list_temp:
+            p.set_net_id(temp_id)
+            probe_check[probe_list.index(p)]=True
+        temp_id=temp_id+1
+
+
+
 class Probe(object):
+    def calc_sector(self):
+        self.sector=(int(math.floor(self.pos[0])), int(math.floor(self.pos[1])))
+    
     def __init__(self, pos, team, ai):
         self.pos = pos
         self.team = team
@@ -154,8 +208,19 @@ class Probe(object):
         self.cargo={'ressources':[0,0,0]}
         self.free_slots=CARGO_SLOTS
         self.act = self.ai.act
+        self.calc_sector()
+        self.network_id=None
     def get_team(self):
         return self.team
+
+    def get_net_id(self):
+        return self.network_id
+
+    def set_net_id(self, net_id):
+        self.network_id = net_id
+
+    def scanned(self):
+        return {'pos':self.pos, 'cargo':self.cargo, 'team_id':self.team.get_id()}
 
     def get_ai(self):
         return self.ai
@@ -170,9 +235,12 @@ class Probe(object):
             self.pos[1]=UNIVERSE_HEIGHT
         elif self.pos[1]<0:
             self.pos[1]=0
-
+        self.calc_sector()
     def get_pos(self):
         return self.pos
+
+    def get_sector(self):
+        return self.sector
 
     def pay_probe(self):
         res=self.cargo['ressources']
@@ -214,12 +282,22 @@ class Team(object):
         return self.id_num
 
 class View(object):
-    def __init__(self, probe):
+    def __init__(self, probe, grid):
         self.pos=copy.deepcopy(probe.get_pos())
         self.cargo=copy.deepcopy(probe.get_cargo())
         self.free_slots=copy.deepcopy(probe.get_free_slots())
         self.team_id=copy.deepcopy(probe.get_team().get_id())
         self.landed=copy.deepcopy(probe.get_landed())
+        self.sector=(int(math.floor(self.pos[0])),int(math.floor(self.pos[1])))
+        self.scans = {'planets':[], 'probes':[]}
+        for x in range(-2,2):
+            if self.sector[0]+x>0 and self.sector[0]+x<UNIVERSE_WIDTH:
+                for y in range(-2,2):
+                    if self.sector[1]+y>0 and self.sector[1]+y<UNIVERSE_HEIGHT:
+                        for planet in grid[self.sector[0]+x][self.sector[1]+y]['planets']:
+                            self.scans['planets'].append(copy.deepcopy(planet.scanned()))
+                        for probe in grid[self.sector[0]+x][self.sector[1]+y]['probes']:
+                            self.scans['probes'].append(copy.deepcopy(probe.scanned()))
 
 class Game(object):
     def __init__(self, ai_list):
@@ -233,6 +311,14 @@ class Game(object):
         self.team_colours.append((255,255,0))
         self.team_colours.append((255,0,255))
         self.team_colours.append((0,255,255))
+        
+
+        #create grid
+        self.grid=[]
+        for x in range(UNIVERSE_WIDTH):
+            self.grid.append([])
+            for y in range(UNIVERSE_HEIGHT):
+                self.grid[x].append({'probes':[], 'planets':[]})
 
         #create planets
         self.planet_list = []
@@ -246,6 +332,7 @@ class Game(object):
         random.shuffle(res)
         planet_temp = Planet([xtemp,ytemp], res)
         self.planet_list.append(planet_temp)
+        self.grid[xtemp][ytemp]['planets'].append(planet_temp)
         for x in range(PLANETS-1):
             while sum(1 for i in self.planet_list if i.get_pos()==[xtemp,ytemp])!=0:
                 xtemp = random.randint(0,UNIVERSE_WIDTH-1)
@@ -259,6 +346,7 @@ class Game(object):
             random.shuffle(res)
             planet_temp = Planet([xtemp,ytemp], res)
             self.planet_list.append(planet_temp)
+            self.grid[xtemp][ytemp]['planets'].append(planet_temp)
             #print (planet_temp.get_pos(), planet_temp.get_res())
         
         #set equal ressources for all homeworlds
@@ -277,7 +365,9 @@ class Game(object):
         #create initial probes
         self.probe_list = []
         for x in range(0, len(ai_list)):
-            self.probe_list.append(Probe([0,0], self.team_list[x], self.ais[x]))
+            temp_probe=Probe([0,0], self.team_list[x], self.ais[x])
+            self.probe_list.append(temp_probe)
+            self.grid[self.planet_list[x].get_pos()[0]][self.planet_list[x].get_pos()[1]]['probes'].append(temp_probe)
             self.planet_list[x].populate(self.probe_list[x])
             self.probe_list[x].set_pos(copy.deepcopy(self.planet_list[x].get_pos()))
             self.probe_list[x].set_landed(True)
@@ -297,10 +387,13 @@ class Game(object):
             if p.is_populated():
                 p.populating_probe().add_ressources(p.get_res())
                 
+        #update networks
+        #assign_networks(self.grid, self.probe_list)
+
         #create probe/view list
         view_list=[]
         for p in self.probe_list:
-            view_list.append((p, View(p)))
+            view_list.append((p, View(p,self.grid)))
         
 
         #create action list
@@ -308,23 +401,54 @@ class Game(object):
         for (p,v) in view_list:
             action_list.append((p, p.act(v)))
             
+        #print action_list[0][1].get_type(), action_list[0][1].get_data()
+            
         
+        #colonize planets
+        for (p,act) in action_list:
+            if act.get_type()==ACT_COLONIZE:
+                #print "ALARM!!!"
+                if len(self.grid[p.get_sector()[0]][p.get_sector()[1]]['planets'])==1:
+                    if self.grid[p.get_sector()[0]][p.get_sector()[1]]['planets'][0].is_populated()==False:
+                        self.grid[p.get_sector()[0]][p.get_sector()[1]]['planets'][0].populate(p)
+                        p.set_pos=p.get_sector
+                        p.set_landed(True)
+
         #build new probes
         for (p,act) in action_list:
             if act.get_type()==ACT_BUILD:
                 if act.get_data()=='probe':
                     res=p.get_cargo()['ressources']
                     if res[0]>=PROBE_COST and res[1]>=PROBE_COST and res[2]>=PROBE_COST:
-                        self.probe_list.append(Probe(copy.deepcopy(p.get_pos()), p.get_team(), p.get_team().get_ai()[1].ProbeAi))
+                        temp_probe=Probe(copy.deepcopy(p.get_pos()), p.get_team(), p.get_team().get_ai()[1].ProbeAi)
+                        self.probe_list.append(temp_probe)
+                        self.grid[p.get_pos()[0]][p.get_pos()[1]]['probes'].append(temp_probe)
+                        #print self.grid
                         p.pay_probe()
 
         #move probes
         for (p,act) in action_list:
             if act.get_type()==ACT_MOVE:
                 pos=p.get_pos()
-                pos[0]=pos[0]+act.get_data()[0]
-                pos[1]=pos[1]+act.get_data()[1]
+                self.grid[int(math.floor(p.get_pos()[0]))][int(math.floor(p.get_pos()[1]))]['probes'].remove(p)
+                betrag=math.sqrt(math.pow(act.get_data()[0],2)+math.pow(act.get_data()[1],2))
+                if betrag>1:
+                    pos[0]=pos[0]+act.get_data()[0]/betrag
+                    pos[1]=pos[1]+act.get_data()[1]/betrag
+                else:
+                    pos[0]=pos[0]+act.get_data()[0]
+                    pos[1]=pos[1]+act.get_data()[1]
+                if pos[0]>=UNIVERSE_WIDTH-1:
+                    pos[0]=UNIVERSE_WIDTH-1
+                if pos[0]<0:
+                    pos[0]=0
+                if pos[1]>=UNIVERSE_HEIGHT-1:
+                    pos[1]=UNIVERSE_HEIGHT-1
+                if pos[1]<0:
+                    pos[1]=0
+               
                 p.set_pos(pos)
+                self.grid[int(math.floor(p.get_pos()[0]))][int(math.floor(p.get_pos()[1]))]['probes'].append(p)
 
         
         #update display
