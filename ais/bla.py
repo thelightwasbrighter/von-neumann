fc5ae437376138da4c0e29cd027f075c0c20a88a
thelightwasbrighter <unterhosenmoerder@gmx.de>
2013-06-03 17:25:14 +0200

    display stuff

--- ais/bla.py
+++ ais/bla.py
@@ -1,5 +1,20 @@
+von_neumann=__import__("von-neumann")
+import math
+import random
 
 class ProbeAi(object):
     def act(self, view):
-        #print view
-        return "bla"
+        #print view.cargo
+        #view.cargo['ressources']=[100,100,100]
+        if view.landed:
+            res=view.cargo['ressources']
+            if res[0]>=10000 and res[1]>=10000 and res[2]>=10000 and view.landed:
+                return von_neumann.Action(von_neumann.ACT_BUILD, 'probe')
+            else:
+                return von_neumann.Action(von_neumann.ACT_IDLE)
+        else:
+            x=random.uniform(-1,1)
+            y=random.uniform(-1,1)
+            betrag=math.sqrt(math.pow(x,2)+math.pow(y,2))
+            vect=[x/betrag,y/betrag]
+            return von_neumann.Action(von_neumann.ACT_MOVE, vect)
--- von-neumann.py
+++ von-neumann.py
@@ -2,6 +2,7 @@
 
 #import ConfigParser
 import random
+import math
 import sys
 import time
 import importlib
@@ -20,6 +21,8 @@ PLANETS = 300
 SCALE = 2
 RES_MAX = 100
 CARGO_SLOTS = 10000
+PROBE_COST = 100
+
 
 class MapLayer(object):
     def __init__(self, width, height, init=0):
@@ -38,7 +41,7 @@ class MapLayer(object):
 
 class Display(object):
     def __init__(self, width, height, scale=2):
-        self.black = 0,0,0
+        self.background = 0,0,0
         self.width, self.height = width, height
         self.scale = scale
         self.size = (self.width*scale , self.height*scale)
@@ -47,9 +50,36 @@ class Display(object):
         pygame.display.set_caption("von Neumann")
         self.text = []
         self.draft_surface=pygame.Surface(self.size)
-
-    def update(self):
+        self.planet_surface=pygame.Surface(self.size)
+        self.probe_surface=pygame.Surface(self.size)
+        self.planet_surface.set_colorkey(self.background)
+        self.probe_surface.set_colorkey(self.background)
+        
+    
+    def set_frame(self, surface):
+        self.screen.blit(surface, (0,0))
         pygame.display.flip()
+    
+    def update(self, planet_list, probe_list):
+        self.draft_surface.fill(self.background)
+        self.planet_surface.fill(self.background)
+        self.probe_surface.fill(self.background)
+        for p in planet_list:
+            if p.is_populated():
+                for x in range(0,SCALE):
+                    for y in range(0,SCALE):
+                        self.planet_surface.set_at((p.get_pos()[0]*SCALE+x,p.get_pos()[1]*SCALE+y),p.populating_probe().get_team().get_colour())
+            else:
+                for x in range(0,SCALE):
+                    for y in range(0,SCALE):
+                        self.planet_surface.set_at((SCALE*p.get_pos()[0]+x,SCALE*p.get_pos()[1]+y),(25,255,255))
+        
+        for p in probe_list:
+            self.probe_surface.set_at((int(math.floor(p.get_pos()[0]))*SCALE,int(math.floor(p.get_pos()[1]))*SCALE),p.get_team().get_colour())
+            
+        self.draft_surface.blit(self.planet_surface, (0,0))
+        self.draft_surface.blit(self.probe_surface, (0,0))
+        self.set_frame(self.draft_surface)
 
     def new_draft(self):
         self.draft_surface.fill((0,0,0))
@@ -59,16 +89,13 @@ class Display(object):
             for y in range(0,SCALE):
                 self.draft_surface.set_at((SCALE*pos[0]+x,SCALE*pos[1]+y),colour)
         
-    def set_frame(self, surface):
-        self.screen.blit(surface, (0,0))
-        pygame.display.flip()
-
+    
     def draw_draft(self):
         self.set_frame(self.draft_surface)
 
 
 # Actions available to an agent on each turn.
-ACT_BUILD, ACT_MOVE, ACT_EAT, ACT_RELEASE, ACT_ATTACK, ACT_LIFT, ACT_DROP = range(7)
+ACT_BUILD, ACT_MOVE, ACT_IDLE = range(3)
 
 class Action(object):
     '''
@@ -130,12 +157,30 @@ class Probe(object):
     def get_team(self):
         return self.team
 
+    def get_ai(self):
+        return self.ai
+
     def set_pos(self, pos):
         self.pos = pos
+        if self.pos[0]>UNIVERSE_WIDTH:
+            self.pos[0]=UNIVERSE_WIDTH
+        elif self.pos[0]<0:
+            self.pos[0]=0
+        if self.pos[1]>UNIVERSE_HEIGHT:
+            self.pos[1]=UNIVERSE_HEIGHT
+        elif self.pos[1]<0:
+            self.pos[1]=0
 
     def get_pos(self):
         return self.pos
 
+    def pay_probe(self):
+        res=self.cargo['ressources']
+        res[0]=res[0]-PROBE_COST
+        res[1]=res[1]-PROBE_COST
+        res[2]=res[2]-PROBE_COST
+        self.cargo['ressources']=res                
+        
     def set_landed(self, landed):
         self.landed=landed
         
@@ -199,10 +244,10 @@ class Game(object):
             res_b=RES_MAX-res_a
         res=[res_a,res_b,0]
         random.shuffle(res)
-        planet_temp = Planet((xtemp,ytemp), res)
+        planet_temp = Planet([xtemp,ytemp], res)
         self.planet_list.append(planet_temp)
         for x in range(PLANETS-1):
-            while sum(1 for i in self.planet_list if i.get_pos()==(xtemp,ytemp))!=0:
+            while sum(1 for i in self.planet_list if i.get_pos()==[xtemp,ytemp])!=0:
                 xtemp = random.randint(0,UNIVERSE_WIDTH-1)
                 ytemp = random.randint(0,UNIVERSE_HEIGHT-1)
             
@@ -212,7 +257,7 @@ class Game(object):
                 res_b=RES_MAX-res_a
             res=[res_a,res_b,0]
             random.shuffle(res)
-            planet_temp = Planet((xtemp,ytemp), res)
+            planet_temp = Planet([xtemp,ytemp], res)
             self.planet_list.append(planet_temp)
             #print (planet_temp.get_pos(), planet_temp.get_res())
         
@@ -232,16 +277,16 @@ class Game(object):
         #create initial probes
         self.probe_list = []
         for x in range(0, len(ai_list)):
-            self.probe_list.append(Probe((0,0), self.team_list[x], self.ais[x]))
+            self.probe_list.append(Probe([0,0], self.team_list[x], self.ais[x]))
             self.planet_list[x].populate(self.probe_list[x])
-            self.probe_list[x].set_pos(self.planet_list[x].get_pos())
+            self.probe_list[x].set_pos(copy.deepcopy(self.planet_list[x].get_pos()))
             self.probe_list[x].set_landed(True)
 
         #generate display
         self.mydisplay = Display(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, SCALE)
-        self.mydisplay.new_draft()
-        self.draw_planets()
-        self.mydisplay.draw_draft()
+        #self.mydisplay.new_draft()
+        #self.draw_planets()
+        #self.mydisplay.draw_draft()
                 
     def tick(self):
         self.rounds=self.rounds+1
@@ -262,14 +307,28 @@ class Game(object):
         action_list = []
         for (p,v) in view_list:
             action_list.append((p, p.act(v)))
+            
         
-                
-    def draw_planets(self):
-        for x in self.planet_list:
-            if x.is_populated():
-                self.mydisplay.draw_planet(x.get_pos(), x.populating_probe().get_team().get_colour())
-            else:
-                self.mydisplay.draw_planet(x.get_pos(), (255,255,255))
+        #build new probes
+        for (p,act) in action_list:
+            if act.get_type()==ACT_BUILD:
+                if act.get_data()=='probe':
+                    res=p.get_cargo()['ressources']
+                    if res[0]>=PROBE_COST and res[1]>=PROBE_COST and res[2]>=PROBE_COST:
+                        self.probe_list.append(Probe(copy.deepcopy(p.get_pos()), p.get_team(), p.get_team().get_ai()[1].ProbeAi))
+                        p.pay_probe()
+
+        #move probes
+        for (p,act) in action_list:
+            if act.get_type()==ACT_MOVE:
+                pos=p.get_pos()
+                pos[0]=pos[0]+act.get_data()[0]
+                pos[1]=pos[1]+act.get_data()[1]
+                p.set_pos(pos)
+
+        
+        #update display
+        self.mydisplay.update(self.planet_list, self.probe_list)
        
             
 def get_ai(name):
@@ -290,9 +349,9 @@ def main():
 
 #pygame.display.flip()
 
-#if __name__ == "__main__":
-main()
-mygame = Game(ai_list)
+if __name__ == "__main__":
+    main()
+    mygame = Game(ai_list)
 
-while 1:
-    mygame.tick()
+    while 1:
+        mygame.tick()
